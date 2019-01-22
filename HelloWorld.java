0c0693445d8e6db62169775f119807ee13a12881
Mr. Bartucz <jobartucz@isd535.org>
2019-01-21 21:55:39 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// don't worry about the strucure for now, just try to fill in the missing code
+// You can test it in Dr. Java or in repl.it
+// If you get stuck, Google is your friend
+
+class Main {
+  public static void main(String[] args) {
+  
+    // Put your code here that prints out "Hello, world!"
+    
+  }
+}
